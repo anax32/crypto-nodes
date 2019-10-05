7bdccc80fa756301db7540c052e64526ad8c5dfe
ed <anax@hotmail.co.uk>
2019-10-05 23:25:28 +0000

    renamed files + env vars

--- transaction-extractor/copy-txcs.sh
+++ transaction-extractor/copy-txcs.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -u
 
-echo "downloading data from '$AWS_BUCKET_NAME/$AWS_FILE_PREFIX/$UPLOADING_HOST' to '$LOCAL_DATA_DIR'"
+echo "downloading data from '$AWS_BUCKET_NAME/$AWS_SOURCE_PREFIX/$UPLOADING_HOST' to '$LOCAL_DATA_DIR'"
 
 aws s3 cp \
-  s3://$AWS_BUCKET_NAME/$AWS_FILE_PREFIX/$UPLOADING_HOST \
+  s3://$AWS_BUCKET_NAME/$AWS_SOURCE_PREFIX/$UPLOADING_HOST \
   $LOCAL_DATA_DIR \
   --recursive
