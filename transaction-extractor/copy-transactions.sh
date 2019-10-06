1cd7c2638c3d3c117e4fd120f33d6e497d6fb336
ed <anax@hotmail.co.uk>
2019-10-06 00:25:46 +0000

    s3 download in yaml

--- transaction-extractor/copy-transactions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -u
-
-echo "downloading data from '$AWS_BUCKET_NAME/$AWS_SOURCE_PREFIX/$UPLOADING_HOST' to '$LOCAL_DATA_DIR'"
-
-aws s3 cp \
-  s3://$AWS_BUCKET_NAME/$AWS_SOURCE_PREFIX/$UPLOADING_HOST \
-  $LOCAL_DATA_DIR \
-  --recursive
