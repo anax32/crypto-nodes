3797f643af4b434beddfad4cc104fef8ede8cb81
ed <anax@hotmail.co.uk>
2019-10-12 11:47:34 +0000

    get unique transaction ids

--- transaction-extractor/get-transaction-ids.sh
+++ transaction-extractor/get-transaction-ids.sh
@@ -3,10 +3,13 @@ set -u
 
 echo "getting transactions"
 
-# separate the addresses and count the number of occurances
+# separate the addressess and transaction ids
+# uniq the transaction ids because we may have
+# multiple inputs/outputs for each transaction
 gzip -d --stdout $LOCAL_DATA_DIR/$ADDRESS_FILE \
   | cut -d' ' -f2 \
   | sort --numeric-sort \
+  | uniq \
   | gzip -9 > $LOCAL_DATA_DIR/$TRANSACTION_FILE
 
 echo "transactions completed"
