34e91f807579f9b1b4857e6eaf057ee5f3115df9
ed <anax@hotmail.co.uk>
2019-10-13 12:50:41 +0000

    refactoring

--- graph-vis/make-graph.py
+++ graph-vis/make-graph.py
@@ -5,11 +5,14 @@ import os
 import sys
 import logging
 
-from pymongo import MongoClient
-from pprint import pprint
+from os.path import basename
+
+from mongodb_reader import from_mongodb
+from text_reader import from_text
 
 import networkx as nx
 
+# setup the log
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -19,68 +22,28 @@ formatter = logging.Formatter("%(asctime)s:%(name)s:%(levelname)s - %(message)s"
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-# build the uri for the connection
-uri = "mongodb://%s:%s@%s:%i" % (os.environ["MONGODB_USER"],
-                                 os.environ["MONGODB_PASS"],
-                                 os.environ["MONGODB_HOST"],
-                                 int(os.environ["MONGODB_PORT"]))
 
-db_name = os.environ["MONGODB_DATABASE"]
-cl_name = os.environ["MONGODB_COLLECTION"]
-node_count = int(os.environ["NODE_COUNT"])
-
-logger.info("using: '%s'" % uri)
-logger.info("processing %i nodes in %s:%s" % (node_count, db_name, cl_name))
-
-# connect
-client = MongoClient(uri)
-db = client[db_name]
-txs = db[cl_name]
-
-# get some stuff
-logger.info("estimated document count: %i" % txs.estimated_document_count())
-
-# query non-coinbase transactions
-query={"vin.coinbase": {"$exists": False}}
-proj={"txid": 1,
-      "vin.txid": 1}
-# alt. to include address information
-proj_w_addr={"txid": 1,
-             "vin.txid": 1,
-             "vin.vout": 1,
-             "vout.scriptPubKey.addresses": 1}
+# get the generators based on the storage environment
+if sys.argv[1] == "mongodb":
+  transaction_edge_fn = from_mongodb
+elif sys.argv[1] == "text":
+  transaction_edge_fn = from_text
 
 # create a graph
 G = nx.Graph()
 
-# add txs to graph
-if node_count is None or node_count < 1:
-  node_count = txs.estimated_document_count()
+# get all transactions from the generator
+# FIXME: wrap this in tqdm, we need the total count?
+for txid, input_txid in transaction_edge_fn():
+  G.add_node(txid)
+  G.add_node(input_txid)
+  G.add_edge(txid, input_txid)
 
-for record in txs.find(query, proj).limit(node_count):
-  #pprint(record)
-  G.add_node(record["txid"])
-
-  for intx in record["vin"]:
-    G.add_node(intx["txid"])
-
-    G.add_edge(record["txid"], intx["txid"])
-
+# output some graph stats
 logger.info("number of nodes: %i" % G.number_of_nodes())
 logger.info("number of edges: %i" % G.number_of_edges())
 
-
-# draw the graph
-import matplotlib.pyplot as plt
-plt.figure(figsize=(15,15))
-
-logger.info("computing layout...")
-# https://networkx.github.io/documentation/stable/reference/generated/networkx.drawing.nx_agraph.graphviz_layout.html
-pos = nx.spring_layout(G)
-#pos = nx.planar_layout(G)
-
-# https://networkx.github.io/documentation/networkx-1.10/reference/generated/networkx.drawing.nx_pylab.draw_networkx.html?highlight=node_size
-nx.draw(G, pos=pos, node_size=1, alpha=0.5)
-output_filename="/output-images/graph.png"
-logger.debug("saving to '%s'" % output_filename)
-plt.savefig(output_filename, dpi=100)
+# save the graph pickle
+pkl_file = "%s.pkl" % basename(os.environ["GRAPH_FILE"])
+logger.info("writing to '%s'" % pkl_file)
+nx.write_gpickle(G, pkl_file)
