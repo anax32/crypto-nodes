980317559d86aac431c634b5f0c422fd19066020
ed <anax@hotmail.co.uk>
2021-05-27 08:56:42 +0100

    build all images script

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -u
 
-docker build -t crypto-nodes.bitcoin -f bitcoin/bitcoind-from-repo/Dockerfile ./bitcoin
+docker build -t anax32/crypto-nodes.bitcoin -f bitcoin/bitcoind-from-repo/Dockerfile ./bitcoin
+
+docker build -t anax32/crypto-nodes.ethereum -f eth/node/Dockerfile ./eth/node
+docker build -t anax32/crypto-nodes.ethereum-classic -f etc/node/Dockerfile ./etc/node
+
+docker build -t anax32/crypto-nodes.ethereum:mine-latest -f eth/mine/Dockerfile ./eth/mine
+docker build -t anax32/crypto-nodes.ethereum-classic:mine-latest -f etc/mine/Dockerfile ./etc/mine
