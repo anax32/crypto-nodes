9026ba349087f4965ba100f958eef61bb65ef81e
ed <anax@hotmail.co.uk>
2021-05-28 20:51:54 +0100

    Added XMR, Dropped ETC
    + added xmr config
    + dropped etc specific containers to reuse eth containers
    + removed default address in eth container

---
+++ run-nodes.sh
@@ -0,0 +1,55 @@
+
+#!/bin/bash
+
+#
+# ethereum
+#
+docker run \
+  -d \
+  --name ethminer \
+  --gpus all \
+  -it \
+  -e ETH_POOL_ADDR=eu1.ethermine.org \
+  -e ETH_ADDR=0x2067000E3ca05c9007A556Ac4efe238805DB7400 \
+  -e WORKER_NAME=work09 \
+  --restart=always \
+  --cpus="0.5" \
+  anax32/crypto-nodes.ethereum:mine-latest
+
+#docker run \
+#  -d \
+#  --name etcminer \
+#  --gpus all \
+#  -it \
+#  -e ETH_POOL_ADDR=eu1-etc.ethermine.org \
+#  -e ETH_ADDR=0x2067000E3ca05c9007A556Ac4efe238805DB7400 \
+#  -e WORKER_NAME=work09 \
+#  --restart=always \
+#  --cpus="0.5" \
+#  anax32/crypto-nodes.ethmine
+
+
+#
+# xmr
+#
+docker run \
+  -d \
+  --name xmrig \
+  -e XMR_POOL_ADDR=pool.supportxmr.com:443 \
+  -e XMR_ADDR=4AJin9Rwi4KE93rgyPKnds569UeXKgxdW7G9vvKUQCKrPYzHQMQmGrFezLq5GuX3Pfjo1wkiHu3jmGRUhRRufjYPBQPtKaH \
+  -e WORKER_NAME=work01 \
+  anax32/crypto-nodes.xmr:cpu-latest \
+  /bin/bash -c 'xmrig --donate-level 2 -o ${XMR_POOL_ADDR} -u ${XMR_ADDR} -k --tls -p ${WORKER_NAME}'
+
+#
+# monitoring
+#
+docker run \
+  -d \
+  --name gpu-monitor \
+  --gpus all \
+  -it \
+  anax32/nvidia-log
+
+# docker logs -f ethminer
+# docker logs -f xmrig
