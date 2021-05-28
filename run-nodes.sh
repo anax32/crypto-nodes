f1ac0ccc19f4ff3e60e18c6bd793fa16a8868922
ed <anax@hotmail.co.uk>
2021-05-28 20:57:46 +0100

    BUGFIX: added default cmd to xmr container

--- run-nodes.sh
+++ run-nodes.sh
@@ -38,8 +38,7 @@ docker run \
   -e XMR_POOL_ADDR=pool.supportxmr.com:443 \
   -e XMR_ADDR=4AJin9Rwi4KE93rgyPKnds569UeXKgxdW7G9vvKUQCKrPYzHQMQmGrFezLq5GuX3Pfjo1wkiHu3jmGRUhRRufjYPBQPtKaH \
   -e WORKER_NAME=work01 \
-  anax32/crypto-nodes.xmr:cpu-latest \
-  /bin/bash -c 'xmrig --donate-level 2 -o ${XMR_POOL_ADDR} -u ${XMR_ADDR} -k --tls -p ${WORKER_NAME}'
+  anax32/crypto-nodes.xmr:cpu-latest
 
 #
 # monitoring
