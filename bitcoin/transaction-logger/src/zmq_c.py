0a70496898f9268791a00299a2e28f6d195d62cb
ed <anax@hotmail.co.uk>
2021-07-18 00:06:28 +0100

    changes to zmq as __main__

--- bitcoin/transaction-logger/src/zmq_c.py
+++ bitcoin/transaction-logger/src/zmq_c.py
@@ -3,6 +3,7 @@ import os
 import itertools
 import logging
 import sys
+import json
 
 from textwriter import TextWriter
 from mongowriter import MongoWriter
@@ -10,10 +11,9 @@ from bitcoindrpc import BitcoinRPC
 from s3_uploader import upload_file_to_s3
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
+handler.setLevel(os.getenv("LOG_LEVEL", "INFO"))
 formatter = logging.Formatter("%(asctime)s:%(name)s:%(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
@@ -58,8 +58,6 @@ def get_transaction_writers():
     writers.append(unpack_and_write_mdb)
 
   if "FILE_LOGGER" in os.environ and int(os.environ["FILE_LOGGER"]) != 0:
-    import json
-
     write_complete_fn = None
 
     if "AWS_BUCKET_NAME" in os.environ and len(os.environ["AWS_BUCKET_NAME"]) > 0:
@@ -90,57 +88,58 @@ def get_transaction_writers():
   return writers
 
 
-"""
-zmq stuff
-"""
-# set up the sockets
-context = zmq.Context()
+if __name__ == "__main__":
+  """
+  zmq stuff
+  """
+  # set up the sockets
+  context = zmq.Context()
 
-socket = context.socket(zmq.SUB)
-socket.setsockopt(zmq.SUBSCRIBE, b"rawtx")
-socket.connect(os.environ["RAWTX_SOURCE_ADDR"])
+  socket = context.socket(zmq.SUB)
+  socket.setsockopt(zmq.SUBSCRIBE, b"rawtx")
+  socket.connect(os.environ["RAWTX_SOURCE_ADDR"])
 
-logger.info("recving from '%s'" % os.environ["RAWTX_SOURCE_ADDR"])
+  logger.info("recving from '%s'" % os.environ["RAWTX_SOURCE_ADDR"])
 
-# get the RPC object
-rpc = BitcoinRPC(os.environ["BITCOIND_RPC_USER"],
-                 os.environ["BITCOIND_RPC_PASSWORD"],
-                 os.environ["BITCOIND_HOST"],
-                 int(os.environ["BITCOIND_PORT"]))
+  # get the RPC object
+  rp c = BitcoinRPC(os.environ["BITCOIND_RPC_USER"],
+                    os.environ["BITCOIND_RPC_PASSWORD"],
+                    os.environ["BITCOIND_HOST"],
+                    int(os.environ["BITCOIND_PORT"]))
 
-# get the writers
-writers = get_transaction_writers()
-logger.info("using tx writers: '%s'" % str(writers))
+  # get the writers
+  writers = get_transaction_writers()
+  logger.info("using tx writers: '%s'" % str(writers))
 
-# endless loop
-try:
-  for tx_idx in itertools.count():
-    logger.debug("waiting on idx %i" % tx_idx)
-    msg = socket.recv_multipart()
-    logger.debug("got body (len: %i (%i, %i))" % (len(msg), len(msg[0]), len(msg[1])))
-    body = msg[1]
+  # endless loop
+  try:
+    for tx_idx in itertools.count():
+      logger.debug("waiting on idx %i" % tx_idx)
+      msg = socket.recv_multipart()
+      logger.debug("got body (len: %i (%i, %i))" % (len(msg), len(msg[0]), len(msg[1])))
+      body = msg[1]
 
-    # decode the transaction
-    try:
-      decoded_hex_string = rpc("decoderawtransaction", body.hex())
-    except Exception as e:
-      logger.exception(e)
-      continue
+      # decode the transaction
+      try:
+        decoded_hex_string = rpc("decoderawtransaction", body.hex())
+      except Exception as e:
+        logger.exception(e)
+        continue
 
-    if decoded_hex_string is None:
-      logger.warning("rpc call returned None (skipping record)")
-      continue
+      if decoded_hex_string is None:
+        logger.warning("rpc call returned None (skipping record)")
+        continue
 
-    # pass the decoded json on to the writers
-    if len(writers) > 0:
-      for writer in writers:
-        writer(decoded_hex_string)
-    else:
-      logger.warning("no writers in use, not logging tx")
+      # pass the decoded json on to the writers
+      if len(writers) > 0:
+        for writer in writers:
+          writer(decoded_hex_string)
+      else:
+        logger.warning("no writers in use, not logging tx")
 
-except KeyboardInterrupt:
-  logger.info("cleanup")
-  context.destroy()
-except Exception as e:
-  logger.exception(e)
-  context.destroy()
+  except KeyboardInterrupt:
+    logger.info("exiting for KeyboardInterrupt")
+    context.destroy()
+  except Exception as e:
+    logger.exception(e)
+    context.destroy()
